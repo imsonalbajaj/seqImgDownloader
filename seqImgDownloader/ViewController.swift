a45394fcd43592575f82a31f05f03d0052b8e41f
Sonal <imsonalbajaj@gmail.com>
2026-08-25 17:37:47 +0530

    Initial Commit

---
+++ seqImgDownloader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seqImgDownloader
+//
+//  Created by Sonal on 25/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
